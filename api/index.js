a754e74d73045555070e57883111f6fab37d3dc9
Mark Bestil <bestilmark@gmail.com>
2024-09-30 16:20:59 +0800

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,16 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+mongoose
+  .connect(process.env.DB)
+  .then(() => console.log("Database connected..."))
+  .catch((err) => console.log(err));
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000...");
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
 });
