c594f3090049a09024e8a30580457522f4288bf1
Mark Bestil <bestilmark@gmail.com>
2024-10-10 17:32:18 +0800

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,49 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">
+        About Sahand Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum
+        iaculis auctor dui, id tincidunt metus sagittis non. Nulla facilisi.
+        Integer quis suscipit ipsum, pulvinar luctus diam. Mauris ullamcorper
+        semper mauris quis tempor. Donec sed tortor vestibulum, semper lectus
+        commodo, mollis neque. Fusce vitae ex rhoncus, cursus nulla id, suscipit
+        est. Quisque blandit efficitur enim non sagittis. Orci varius natoque
+        penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Praesent tempus dolor quam, non consequat quam aliquet nec. Nunc a magna
+        vitae tortor porttitor gravida eget a lacus. Maecenas auctor magna
+        lorem, vel facilisis mauris tristique id. Nunc pretium justo libero, eu
+        lobortis augue faucibus vel. Aliquam semper pharetra lacus, eget
+        molestie urna. Sed lectus magna, aliquam non est non, tempor auctor
+        elit. Etiam dignissim, sem quis ultrices sollicitudin, urna lorem varius
+        ante, ac tincidunt mauris massa in libero. In pulvinar mauris aliquam
+        lectus hendrerit, commodo fringilla neque lobortis. Vestibulum
+        scelerisque tortor maximus, posuere nunc ac, lacinia sem. Proin
+        efficitur at velit a scelerisque. Vestibulum purus metus, commodo quis
+        velit non, consectetur vestibulum ipsum. Cras pharetra nisl eu feugiat
+        maximus. Integer dignissim, enim eget faucibus hendrerit, orci ex
+        posuere urna, at molestie urna leo nec sem.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Aenean a lorem bibendum, commodo felis vel, interdum mi. Integer ac
+        porttitor magna. Etiam quam ligula, pellentesque nec arcu ac,
+        scelerisque sollicitudin lectus. Morbi euismod, arcu in commodo
+        bibendum, diam mauris cursus dolor, non finibus nulla leo in justo.
+        Praesent et posuere urna. Curabitur luctus sit amet eros sed sodales.
+        Suspendisse neque nibh, mollis eget tellus non, tempor accumsan velit.
+        Donec accumsan nisi in odio pharetra, vel rhoncus libero tempor. Proin
+        in dapibus mi. Integer malesuada interdum augue, id porta quam finibus
+        venenatis. Suspendisse gravida ex quis nunc condimentum, non dignissim
+        enim interdum. Suspendisse a sodales felis, lobortis mattis velit.
+        Nullam posuere nibh sapien, eget tempus arcu malesuada auctor. Morbi a
+        erat pharetra, dictum nibh eu, ornare urna.
+      </p>
+    </div>
+  );
 }
